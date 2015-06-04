17c92949a86cb452f17a68ad7d786d0ab422be24
Corey Sewell <corey@gdmedia.tv>
2015-06-04 14:51:47 +1200

    Updated to use google api v3

--- code/lib/YTVideoEntry.php
+++ code/lib/YTVideoEntry.php
@@ -11,17 +11,30 @@ class YTVideoEntry extends ViewableData {
     public $URL;
 
     function __construct() {
-
+        
     }
 
     public function fortemplate() {
         return $this->renderWith(__CLASS__);
     }
 
-//    public function GetCurrentController(){
-//        return Controller::curr();
-//    }
-//    
-
+    /**
+     * 
+     * @param Google_Service_YouTube_SearchResultSnippet $snippit
+     * @return \YTVideoEntry
+     */
+    static function fromYouTubeSearchResultSnippet(Google_Service_YouTube_SearchResultSnippet $snippit) {
+        $result = new YTVideoEntry();
+        if ($snippit instanceof Google_Service_YouTube_Video) {
+            $result->ID = $snippit['id'];
+        } else {
+            $result->ID = $snippit['id']['videoId'];
+        }
+        $result->Thumb       = $snippit['snippet']['thumbnails']['high']['url'];
+        $result->Title       = $snippit['snippet']['title'];
+        $result->Description = $snippit['snippet']['description'];
+        $result->URL         = Controller::curr()->Link('ViewVideo/' . $result->ID);
+        return $result;
+    }
 
 }
--- code/pages/YouTubePage.php
+++ code/pages/YouTubePage.php
@@ -1,166 +1,155 @@
 <?php
 
 class YouTubePage extends Page {
-
+    
 }
 
 class YouTubePage_Controller extends Page_Controller {
 
-    var $yt;
-
     /**
-     * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-     * permissions or conditions required to allow the user to access it.
      *
-     * <code>
-     * array (
-     *     'action', // anyone can access this action
-     *     'action' => true, // same as above
-     *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-     *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-     * );
-     * </code>
-     *
-     * @var array
+     * @var Google_Service_YouTube
      */
+    private $yt;
+    private $conf;
+    private $videos;
+    private $NextPageToken;
+    private $PrevPageToken;
+    private $TotalResults;
+    private $ResultsPerPage;
+    private $defaults               = array();
     private static $allowed_actions = array(
         'ViewVideo'
     );
+    protected $validDefaults        = array(
+        'safeSearch' => array("moderate", "none", "strict")
+    );
 
     public function init() {
-        set_include_path(get_include_path() . PATH_SEPARATOR . BASE_PATH . "/vendor/zend/gdata/library/");
-        $this->yt = new Zend_Gdata_YouTube();
-        $this->yt->setMajorProtocolVersion(2);
+        $this->yt     = new Google_Service_YouTube($this->getGoogleClient());
+        $this->setDefaults();
+        $this->videos = $this->search($this->GetSearchTerm(), $this->GetSearchType());
         parent::init();
     }
 
-    public function search($term, $type = "all", $startIndex = "", $maxResults = "") {
-        $ytQuery = $this->yt->newVideoQuery();
-        $ytQuery->setQuery($term);
-        $ytQuery->setStartIndex($startIndex);
-        $ytQuery->setMaxResults($maxResults);
-        $ytQuery->setFormat(5);
-        $ytQuery->SetSafeSearch('strict');
-
-        $feed = array();
-        /* check for one of the standard feeds, or list from 'all' videos */
-        switch ($type) {
-            case 'most_viewed':
-                $ytQuery->setFeedType('most viewed');
-                $ytQuery->setTime('this_week');
-                $feed = $this->yt->getVideoFeed($ytQuery);
-                break;
-            case 'most_recent':
-                $ytQuery->setFeedType('most recent');
-                $feed = $this->yt->getVideoFeed($ytQuery);
-                break;
-            case 'recently_featured':
-                $ytQuery->setFeedType('recently featured');
-                $feed = $this->yt->getVideoFeed($ytQuery);
-                break;
-            case 'top_rated':
-                $ytQuery->setFeedType('top rated');
-                $ytQuery->setTime('this_week');
-                $feed = $this->yt->getVideoFeed($ytQuery);
-                break;
-            case 'all':
-                $feed = $this->yt->getVideoFeed($ytQuery);
-                break;
-            default:
-                echo 'ERROR - unknown queryType - "' . $type . '"';
-                break;
+    protected function getGoogleClient() {
+        $client       = new Google_Client();
+        $this->conf   = $this->config()->forClass("YouTubePage");
+        $appName      = $this->conf->get("AppName");
+        $auth         = $this->conf->get("Auth");
+        $developerKey = isset($auth['DeveloperKey']) ? $auth['DeveloperKey'] : false;
+        if (empty($appName)) {
+            throw new Exception("No AppName in YouTubePage Config");
+        } else if (empty($auth)) {
+            throw new Exception("No Auth in YouTubePage Config");
         }
-        $result = $this->entryToVideo($feed);
+        $client->setApplicationName($appName);
+        if (!empty($developerKey)) {
+            $client->setDeveloperKey($developerKey);
+        } else {
+            throw new Exception("No Auth DeveloperKey set in YouTubePage Config");
+        }
+        return $client;
+    }
+
+    protected function setDefaults() {
+        $defaults = $this->conf->get("Defaults");
+        if ($defaults) {
+            foreach ($defaults as $key => $value) {
+                if (isset($this->validDefaults[$key]) && in_array($value, $this->validDefaults[$key])) {
+                    $this->defaults[$key] = $value;
+                }
+            }
+        }
+    }
+
+    public function getPagerLink($token) {
+        $result = false;
+        if ($token) {
+            $requestVars              = $this->request->getVars();
+            unset($requestVars['url']);
+            $requestVars["pageToken"] = $token;
+            $result                   = htmlentities($this->Link("?" . http_build_query($requestVars)));
+        }
         return $result;
     }
 
-    /**
-     * Returns a feed of top rated videos for the specified user
-     *
-     * @param  string $user The username
-     * @return Zend_Gdata_YouTube_VideoFeed The feed of top rated videos
-     */
-    function getTopRatedVideosByUser($userUri) {
-        $userVideosUrl = $userUri . '/uploads';
-        $ytQuery       = $this->yt->newVideoQuery($userVideosUrl);
-        $ytQuery->SetSafeSearch('strict');
-        // order by the rating of the videos
-        $ytQuery->setOrderBy('rating');
-        // retrieve a maximum of 5 videos
-        $ytQuery->setMaxResults(5);
-        // retrieve only embeddable videos
-        $ytQuery->setFormat(5);
-        $result        = $this->entryToVideo($this->yt->getVideoFeed($ytQuery));
-        return $result;
+    public function getNextPageLink() {
+        return $this->getPagerLink($this->NextPageToken);
     }
 
-    /**
-     * Returns a feed of videos related to the specified video
-     *
-     * @param  string $videoId The video
-     * @return Zend_Gdata_YouTube_VideoFeed The feed of related videos
-     */
-    function getRelatedVideos($videoId) {
-        $ytQuery = $this->yt->newVideoQuery();
-        // show videos related to the specified video
-        $ytQuery->SetSafeSearch('strict');
-        $ytQuery->setFeedType('related', $videoId);
-        // order videos by rating
-        $ytQuery->setOrderBy('rating');
-        // retrieve a maximum of 5 videos
-        $ytQuery->setMaxResults(5);
-        // retrieve only embeddable videos
-        $ytQuery->setFormat(5);
-        $result  = $this->entryToVideo($this->yt->getVideoFeed($ytQuery));
+    public function getPrevPageLink() {
+        return $this->getPagerLink($this->PrevPageToken);
+    }
+
+    public function search($term, $type = "all", $maxResults = 9) {
+        $result = new ArrayList();
+
+        $options = array(
+            'q'               => $term,
+            'maxResults'      => $maxResults,
+            'type'            => "video",
+            'videoEmbeddable' => 'true',
+            'order'           => $type,
+        );
+        if ($this->GetPageToken()) {
+            $options['pageToken'] = $this->GetPageToken();
+        }
+        if ($this->GetCategory()) {
+            $options['videoCategoryId'] = $this->GetCategory();
+        }
+        if (is_array($this->defaults)) {
+            $options = array_merge($this->defaults, $options);
+        }
+        echo '<pre class="debug"> "$options"' . PHP_EOL . print_r($options, true) . PHP_EOL . '</pre>';
+        $searchResponse = $this->yt->search->listSearch("snippet", $options);
+        foreach ($searchResponse['items'] as $searchResult) {
+            $result->add(YTVideoEntry::fromYouTubeSearchResultSnippet($searchResult));
+        }
+        $this->NextPageToken  = $searchResponse["nextPageToken"];
+        $this->PrevPageToken  = $searchResponse["prevPageToken"];
+        $this->TotalResults   = $searchResponse["pageInfo"]["totalResults"];
+        $this->ResultsPerPage = $searchResponse["pageInfo"]["resultsPerPage"];
+
         return $result;
     }
 
     public function getVideos() {
-        $videos = $this->search($this->GetSearchTerm(), $this->GetSearchType());
-        return $videos;
-    }
-
-    /**
-     *
-     * @param type $entry
-     * @return \ArrayList|YTVideoEntry
-     */
-    public function entryToVideo($entry) {
-        $result = new YTVideoEntry();
-
-        if ($entry instanceof Zend_Gdata_YouTube_VideoFeed) {
-            $result = new ArrayList();
-            foreach ($entry as $singleEntry) {
-                $result->add($this->entryToVideo($singleEntry));
-            }
-        } else if ($entry instanceof Zend_Gdata_YouTube_VideoEntry) {
-            $result->ID          = $entry->getVideoId();
-            $result->Thumb       = $entry->mediaGroup->thumbnail[0]->url;
-            $result->Title       = htmlentities($entry->mediaGroup->title);
-            $result->Description = htmlentities($entry->mediaGroup->description);
-            $result->Author      = htmlentities($entry->author[0]->name);
-            $result->URL         = $this->Link('ViewVideo/' . $result->ID);
-            $result->AuthorUri   = $entry->author[0]->uri;
-        }
-        return $result;
+        return $this->videos;
     }
 
     public function ViewVideo(SS_HTTPRequest $request) {
-        $videoId          = $request->param("ID");
-        $entry            = $this->yt->getVideoEntry($videoId);
-        $video            = $this->entryToVideo($entry);
-        $relatedVideoFeed = $this->getRelatedVideos($video->ID);
-        $topRatedFeed     = $this->getTopRatedVideosByUser($video->AuthorUri);
-        return $this->customise(array("Video" => $video, "RelatedVideos" => $relatedVideoFeed, "TopRated" => $topRatedFeed))->renderWith(array('YouTubePage_view', 'Page'));
+        $relatedVideoFeed = new ArrayList();
+
+        $videoId         = $request->param("ID");
+        $options         = ['maxResults' => 1, 'id' => $videoId];
+        $searchResponse  = $this->yt->videos->listVideos("snippet", $options);
+        $relatedResponse = $this->yt->search->listSearch("snippet", ['maxResults' => 16, 'type' => 'video', 'relatedToVideoId' => $videoId]);
+        foreach ($relatedResponse['items'] as $searchResult) {
+            $relatedVideoFeed->add(YTVideoEntry::fromYouTubeSearchResultSnippet($searchResult));
+        }
+        return $this->customise(array(
+                    "Video"         => YTVideoEntry::fromYouTubeSearchResultSnippet($searchResponse[0]),
+                    "RelatedVideos" => $relatedVideoFeed
+                        )
+                )->renderWith(array('YouTubePage_view', 'Page'));
     }
 
     public function SearchForm() {
-        $searchText = $this->GetSearchTerm() ? : "";
-
-        $searchField          = new TextField('Search', "Search youtube for:", $searchText);
+        $searchText  = $this->GetSearchTerm() ? : "";
+        $queryType   = $this->GetSearchType() ? : "";
+        $category    = $this->GetCategory() ? : "";
+        $searchField = new TextField('Search', "Search youtube for:", $searchText);
         $searchField->setAttribute('placeholder', "Type here");
+
         $fields               = new FieldList(
-                new DropdownField('queryType', "Search type:", array('all' => 'All Videos', 'top_rated' => 'Top Rated Videos', 'most_viewed' => 'Most Viewed Videos', 'recently_featured' => 'Recently Featured Videos')), $searchField
+                new DropdownField('queryType', "Search type:", array(
+            'relevance' => 'All Videos',
+            'viewcount' => 'Most Viewed Videos',
+            'date'      => 'Most recent',
+            'rating'    => 'Top rated',
+                ), $queryType), new DropdownField('category', "Category:", $this->GetCategories()
+                , $category), $searchField
         );
         $action               = new FormAction('YouTubeSearchResults', _t('SearchForm.GO', 'Go'));
         $action->addExtraClass('btn btn-default btn-search');
@@ -185,9 +174,36 @@ class YouTubePage_Controller extends Page_Controller {
     }
 
     public function GetSearchType() {
-        $result = "recently_featured";
+        $result = "relevance";
         if ($this->request && $this->request->getVar('queryType')) {
             $result = $this->request->getVar('queryType');
+        }
+        return $result;
+    }
+
+    public function GetCategory() {
+        $result = "";
+        if ($this->request && $this->request->getVar('category')) {
+            $result = $this->request->getVar('category');
+        }
+        return $result;
+    }
+
+    public function GetPageToken() {
+        $result = false;
+        if ($this->request && $this->request->getVar('pageToken')) {
+            $result = $this->request->getVar('pageToken');
+        }
+        return $result;
+    }
+
+    public function GetCategories() {
+        $localeEx   = explode("_", Member::currentUser()->Locale);
+        $regionCode = isset($localeEx[1]) ? $localeEx[1] : "NZ";
+        $categories = $this->yt->videoCategories->listVideoCategories('snippet', ['regionCode' => $regionCode]);
+        $result     = array("" => "Any");
+        foreach ($categories as $category) {
+            $result[$category->getId()] = $category['snippet']['title'];
         }
         return $result;
     }
